56c69f56445559539508306559afb54ce189c74d
ejnsa <dilipdasheadteacher811@gmail.com>
2026-06-06 22:31:26 +0600

    Add files via upload

--- Demo.js
+++ Demo.js
@@ -72,167 +72,167 @@ window.addEventListener('load', () => {
     }
     
     // Drop the 2nd post into the 2nd box (if it exists)
-    else if (postsArray[1]) {
+    if (postsArray[1]) {
         postShower2.innerHTML = postsArray[1];
     }
     
     // Drop the 3rd post into the 3rd box (if it exists)
-    else if (postsArray[2]) {
+    if (postsArray[2]) {
         postShower3.innerHTML = postsArray[2];
     }
     
     // Drop the 4th post into the 4th box (if it exists)
-    else if (postsArray[3]) {
+    if (postsArray[3]) {
         postShower4.innerHTML = postsArray[3];
     }
-    else if (postsArray[4]) {
+    if (postsArray[4]) {
         postShower5.innerHTML = postsArray[4];
     }
-    else if (postsArray[5]) {
+    if (postsArray[5]) {
         postShower6.innerHTML = postsArray[5];
     }
-    else if (postsArray[6]) {
+    if (postsArray[6]) {
         postShower7.innerHTML = postsArray[6];
     }
-    else if (postsArray[7]) {
+    if (postsArray[7]) {
         postShower8.innerHTML = postsArray[7];
     }
-    else if (postsArray[8]) {
+    if (postsArray[8]) {
         postShower9.innerHTML = postsArray[8];
     }
-    else if (postsArray[9]) {
+    if (postsArray[9]) {
         postShower10.innerHTML = postsArray[9];
     }
-    else if (postsArray[10]) {
+    if (postsArray[10]) {
         postShower11.innerHTML = postsArray[10];
     }
-    else if (postsArray[11]) {
+    if (postsArray[11]) {
         postShower12.innerHTML = postsArray[11];
     }
-    else if (postsArray[12]) {
+    if (postsArray[12]) {
         postShower13.innerHTML = postsArray[12];
     }
-    else if (postsArray[13]) {
+    if (postsArray[13]) {
         postShower14.innerHTML = postsArray[13];
     }
-    else if (postsArray[14]) {
+    if (postsArray[14]) {
         postShower15.innerHTML = postsArray[14];
     }
-    else if (postsArray[15]) {
+    if (postsArray[15]) {
         postShower16.innerHTML = postsArray[15];
     }
-    else if (postsArray[16]) {
+    if (postsArray[16]) {
         postShower17.innerHTML = postsArray[16];
     }
-    else if (postsArray[17]) {
+    if (postsArray[17]) {
         postShower18.innerHTML = postsArray[17];
     }
-    else if (postsArray[18]) {
+    if (postsArray[18]) {
         postShower19.innerHTML = postsArray[18];
     }
-    else if (postsArray[19]) {
+    if (postsArray[19]) {
         postShower20.innerHTML = postsArray[19];
     }
-    else if (postsArray[20]) {
+    if (postsArray[20]) {
         postShower21.innerHTML = postsArray[20];
     }
-    else if (postsArray[21]) {
+    if (postsArray[21]) {
         postShower22.innerHTML = postsArray[21];
     }
-    else if (postsArray[22]) {
+    if (postsArray[22]) {
         postShower23.innerHTML = postsArray[22];
     }
-    else if (postsArray[23]) {
+    if (postsArray[23]) {
         postShower24.innerHTML = postsArray[23];
     }
-    else if (postsArray[24]) {
+    if (postsArray[24]) {
         postShower25.innerHTML = postsArray[24];
     }
-    else if (postsArray[25]) {
+    if (postsArray[25]) {
         postShower26.innerHTML = postsArray[25];
     }
-    else if (postsArray[26]) {
+    if (postsArray[26]) {
         postShower27.innerHTML = postsArray[26];
     }
-    else if (postsArray[27]) {
+    if (postsArray[27]) {
         postShower28.innerHTML = postsArray[27];
     }
-    else if (postsArray[28]) {
+    if (postsArray[28]) {
         postShower29.innerHTML = postsArray[28];
     }
-    else if (postsArray[29]) {
+    if (postsArray[29]) {
         postShower30.innerHTML = postsArray[29];
     }
-    else if (postsArray[30]) {
+    if (postsArray[30]) {
         postShower31.innerHTML = postsArray[30];
     }
-    else if (postsArray[31]) {
+    if (postsArray[31]) {
         postShower32.innerHTML = postsArray[31];
     }
-    else if (postsArray[32]) {
+    if (postsArray[32]) {
         postShower33.innerHTML = postsArray[32];
     }
-    else if (postsArray[33]) {
+    if (postsArray[33]) {
         postShower34.innerHTML = postsArray[33];
     }
-    else if (postsArray[35]) {
+    if (postsArray[35]) {
         postShower36.innerHTML = postsArray[35];
     }
-    else if (postsArray[36]) {
+    if (postsArray[36]) {
         postShower37.innerHTML = postsArray[36];
     }
-    else if (postsArray[37]) {
+    if (postsArray[37]) {
         postShower38.innerHTML = postsArray[37];
     }
-    else if (postsArray[38]) {
+    if (postsArray[38]) {
         postShower39.innerHTML = postsArray[38];
     }
-    else if (postsArray[39]) {
+    if (postsArray[39]) {
         postShower40.innerHTML = postsArray[39];
     }
-    else if (postsArray[40]) {
+    if (postsArray[40]) {
         postShower41.innerHTML = postsArray[40];
     }
-    else if (postsArray[42]) {
+    if (postsArray[42]) {
         postShower42.innerHTML = postsArray[42];
     }
-    else if (postsArray[43]) {
+    if (postsArray[43]) {
         postShower43.innerHTML = postsArray[43];
     }
-    else if (postsArray[44]) {
+    if (postsArray[44]) {
         postShower44.innerHTML = postsArray[44];
     }
-    else if (postsArray[45]) {
+    if (postsArray[45]) {
         postShower45.innerHTML = postsArray[45];
     }
-    else if (postsArray[46]) {
+    if (postsArray[46]) {
         postShower46.innerHTML = postsArray[46];
     }
-    else if (postsArray[47]) {
+    if (postsArray[47]) {
         postShower47.innerHTML = postsArray[47];
     }
-    else if (postsArray[48]) {
+    if (postsArray[48]) {
         postShower48.innerHTML = postsArray[48];
     }
-    else if (postsArray[49]) {
+    if (postsArray[49]) {
         postShower49.innerHTML = postsArray[49];
     }
-    else if (postsArray[50]) {
+    if (postsArray[50]) {
         postShower50.innerHTML = postsArray[50];
     }
-    else if (postsArray[51]) {
+    if (postsArray[51]) {
         postShower51.innerHTML = postsArray[51];
     }
-    else if (postsArray[52]) {
+    if (postsArray[52]) {
         postShower52.innerHTML = postsArray[52];
     }
-    else if (postsArray[53]) {
+    if (postsArray[53]) {
         postShower53.innerHTML = postsArray[53];
     }
-    else if (postsArray[54]) {
+    if (postsArray[54]) {
         postShower54.innerHTML = postsArray[54];
     }
-    else if (postsArray[55]) {
+    if (postsArray[55]) {
         postShower55.innerHTML = postsArray[55];
     }
 
